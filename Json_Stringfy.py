a9a1dfd4b64fa76bc9cdce7b02197643bfd55435
Arsal Azeem <36434007+arsalazeem@users.noreply.github.com>
2021-03-22 18:10:42 +0500

    Update Json_Stringfy.py

--- Json_Stringfy.py
+++ Json_Stringfy.py
@@ -2,21 +2,11 @@ import json
 import pdb
 
 
-class Employee:
-    def __init__(self, name, salary, address):
-        self.name = name
-        self.salary = salary
-        self.address = address
+
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
 
-
-class Address:
-    def __init__(self, city, street, pin):
-        self.city = city
-        self.street = street
-        self.pin = pin
 
 
 
