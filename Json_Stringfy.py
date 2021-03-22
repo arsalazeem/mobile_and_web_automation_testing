eeb98a8f66e830855cf1a98f9a1636fbd62b5a91
Arsal Azeem <36434007+arsalazeem@users.noreply.github.com>
2021-03-22 18:10:22 +0500

    Update Json_Stringfy.py

--- Json_Stringfy.py
+++ Json_Stringfy.py
@@ -19,15 +19,4 @@ class Address:
         self.pin = pin
 
 
-address = Address("Alpharetta", "7258 Spring Street", "30004")
-employee = Employee("John", 9000, address)
-print(type(employee))
-pdb.set_trace()
-print("Encode into JSON formatted Data")
-employeeJSONData = json.dumps(employee.toJson(), indent=4)
-print(employeeJSONData)
 
-# Let's load it using the load method to check if we can decode it or not.
-print("Decode JSON formatted Data")
-employeeJSON = json.loads(employeeJSONData)
-print(employeeJSON)
